d50f6059c11a15031629bcdfadd6ee808d2a41ea
David Davis <davisd@davisd.com>
2011-11-08 16:40:09 -0600

    refactored

--- adminwidgetswap.py
+++ adminwidgetswap.py
@@ -6,44 +6,32 @@ from django.contrib import admin
 # autodiscover the admin, very important
 admin.autodiscover()
 
-def __get_model_formfield_for_dbfield(model, field, widget):
-    """
-    Gets the new formfield_for_dbfield_function for a model
-    """
-    old_formfield_for_dbfield = admin.site._registry[model].formfield_for_dbfield
-    def formfield_for_dbfield(db_field, **kwargs):
-        if db_field.name == field:
-            kwargs['widget'] = widget
-        return old_formfield_for_dbfield(db_field, **kwargs)
-    return formfield_for_dbfield
-
-def __get_inline_formfield_for_dbfield(inline, field, widget):
-    """
-    Gets the new formfield_for_dbfield function for an inline
-    """
-    old_formfield_for_dbfield = inline.formfield_for_dbfield
-    def formfield_for_dbfield(db_field, **kwargs):
-        if db_field.name == field:
-            kwargs['widget'] = widget
-        return old_formfield_for_dbfield(db_field, **kwargs)
-    return formfield_for_dbfield
-
 def swap_model_field(model, field, widget):
     """
     Swaps an admin model field widget (not the inlines where the model is used)
     """
     if admin.site._registry.has_key(model):
-        admin.site._registry[model].formfield_for_dbfield = __get_model_formfield_for_dbfield(model, field, widget)
+        old_formfield_for_dbfield = admin.site._registry[model].formfield_for_dbfield
+        def formfield_for_dbfield(db_field, **kwargs):
+            if db_field.name == field:
+                kwargs['widget'] = widget
+            return old_formfield_for_dbfield(db_field, **kwargs)
+
+        admin.site._registry[model].formfield_for_dbfield = formfield_for_dbfield
     
 def swap_model_inline_field(model, field, widget):
     """
     Swaps admin model inline field widget (not the direct model admin)
     """
     for registered_model in admin.site._registry:
-        if admin.site._registry.has_key(registered_model):
-            for inline in admin.site._registry[registered_model].inline_instances:
-                if inline.model == model:
-                    inline.formfield_for_dbfield = __get_inline_formfield_for_dbfield(inline, field, widget)
+        for inline in admin.site._registry[registered_model].inlines:
+            if inline.model == model:
+                old_formfield_for_dbfield=inline.formfield_for_dbfield
+                def formfield_for_dbfield(instance, db_field, *args, **kwargs):
+                    if db_field.name == field:
+                        kwargs['widget'] = widget
+                    return old_formfield_for_dbfield(instance, db_field, *args, **kwargs)
+                inline.formfield_for_dbfield = formfield_for_dbfield
 
 def swap_model_and_inline_fields(model, field, widget):
     """
@@ -51,3 +39,4 @@ def swap_model_and_inline_fields(model, field, widget):
     """
     swap_model_field(model, field, widget)
     swap_model_inline_field(model, field, widget)
+
